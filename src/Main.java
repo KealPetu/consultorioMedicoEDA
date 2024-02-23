9b1ebe2cc05118b449f475bb8be40491cfca327a
KealPetu <kpenafiel99@gmail.com>
2024-02-23 13:50:58 -0500

    Programa de consultorio medico usando estructuras de datos

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Group root = new Group();
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+}
