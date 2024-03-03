719c919efe6643f68ec931c735868b76d1f07e62
KealPetu <kpenafiel99@gmail.com>
2024-03-03 00:00:26 -0500

    Refactorizacion de metodos

--- src/frontend/MenuRegistro.java
+++ src/frontend/MenuRegistro.java
@@ -4,11 +4,19 @@ import backend.ListaDeMedicos;
 import backend.Medico;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
 
 import java.io.*;
 
 public class MenuRegistro {
+    private Parent root;
+    private Scene scene;
+    private Stage stage;
     @FXML
     private TextField nombre;
     @FXML
@@ -27,47 +35,77 @@ public class MenuRegistro {
     @FXML
     private Label errorUsuarioExistente,
             errorContrasenaNoCoincide;
+    private File file = new File("listaDeMedicos");
 
     @FXML
     public void registrarNuevoMedico(ActionEvent event) throws IOException {
-        if (!contrasena.getText().equals(contrasenaRepetida.getText())){
-            showPswrdErrorLabel();
-            return;
-        }
 
-        ListaDeMedicos medicos = null;
-        File file = new File("listaDeMedicos");
+        if (contrasenasNoSonIguales()) return;
+
         try{
 
-            FileInputStream fileIn = new FileInputStream(file);
-            ObjectInputStream objectIn = new ObjectInputStream(fileIn);
-            medicos = (ListaDeMedicos) objectIn.readObject();
-
-            if (medicos.containsKey(cedula.getText())){
-                showUserErrorLabel();
-                return;
-            }
-
-            FileOutputStream fileOutputStream = new FileOutputStream(file);
-            ObjectOutputStream objectOutput = new ObjectOutputStream(fileOutputStream);
-            medicos.put(cedula.getText(), new Medico(nombre.getText(), apellido.getText(), fechaDeNacimiento.getAccessibleText(), contrasena.getText()));
-            objectOutput.writeObject(medicos);
-            objectOutput.close();
-            fileOutputStream.close();
+            ListaDeMedicos medicos = getListaDeMedicos();
+            if (yaExisteMedico(medicos)) return;
+            escribirEnElArchivo(medicos);
 
         }catch (FileNotFoundException e) {
 
-            FileOutputStream fileOutputStream = new FileOutputStream(file);
-            ObjectOutputStream objectOutput = new ObjectOutputStream(fileOutputStream);
-            medicos = new ListaDeMedicos();
-            medicos.put(cedula.getText(), new Medico(nombre.getText(), apellido.getText(), fechaDeNacimiento.getAccessibleText(), contrasena.getText()));
-            objectOutput.writeObject(medicos);
-            objectOutput.close();
-            fileOutputStream.close();
+            crearArchivo();
 
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @FXML
+    public void regresarAMenuLogIn(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("../frontend/MenuLogIn.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private boolean contrasenasNoSonIguales() {
+        if (!contrasena.getText().equals(contrasenaRepetida.getText())){
+            showPswrdErrorLabel();
+            return true;
+        }
+        return false;
+    }
+
+    private void crearArchivo() throws IOException {
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
+        ObjectOutputStream objectOutput = new ObjectOutputStream(fileOutputStream);
+        ListaDeMedicos medicos = new ListaDeMedicos();
+        medicos.put(cedula.getText(), new Medico(nombre.getText(), apellido.getText(), fechaDeNacimiento.getAccessibleText(), contrasena.getText()));
+        objectOutput.writeObject(medicos);
+        objectOutput.close();
+        fileOutputStream.close();
+    }
+
+    private void escribirEnElArchivo(ListaDeMedicos medicos) throws IOException {
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
+        ObjectOutputStream objectOutput = new ObjectOutputStream(fileOutputStream);
+        medicos.put(cedula.getText(), new Medico(nombre.getText(), apellido.getText(), fechaDeNacimiento.getAccessibleText(), contrasena.getText()));
+        objectOutput.writeObject(medicos);
+        objectOutput.close();
+        fileOutputStream.close();
+    }
+
+    private boolean yaExisteMedico(ListaDeMedicos medicos) {
+        if (medicos.containsKey(cedula.getText())){
+            showUserErrorLabel();
+            return true;
+        }
+        return false;
+    }
+
+    private ListaDeMedicos getListaDeMedicos() throws IOException, ClassNotFoundException {
+        FileInputStream fileIn = new FileInputStream(file);
+        ObjectInputStream objectIn = new ObjectInputStream(fileIn);
+        ListaDeMedicos medicos = (ListaDeMedicos) objectIn.readObject();
+        return medicos;
     }
 
     private void showPswrdErrorLabel() {
